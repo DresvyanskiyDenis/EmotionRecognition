0464f578cdae464e298a65c1e82f9ab12aa485b0
Denis <ecodenis@yandex.ru>
2022-12-13 19:42:15 +0100

    A new script for the preprocessing of the SEWA dataset (labels and face extraction) has been created. It is done and tested. The script takes the functions from the RECOLA.py script, since the preprocessing procedure is similar.

---
+++ src/data_preprocessing/SEWA.py
@@ -0,0 +1,35 @@
+import os
+
+import pandas as pd
+
+from src.data_preprocessing.RECOLA import load_labels, extract_faces_with_filenames_in_dataframe
+
+
+def main():
+    # here we use the functions from the RECOLA.py file, since the preprocessing procedure is the same for SEMAINE as well
+    # all we need is to modify paths to data, labels, and so on.
+    path_to_data = r"G:\Datasets\SEWA\Original\videos"
+    output_path = r"G:\Datasets\SEWA\preprocessed"
+    path_to_arousal_labels = r"G:\Datasets\SEWA\SEW_labels_arousal_100Hz_gold_shifted.csv"
+    path_to_valence_labels = r"G:\Datasets\SEWA\SEW_labels_valence_100Hz_gold_shifted.csv"
+    # load arousal and valence labels
+    arousal_labels = load_labels(path_to_arousal_labels)
+    arousal_labels.columns = ["filename", "timestamp", "arousal"]
+    valence_labels = load_labels(path_to_valence_labels)
+    valence_labels.columns = ["filename", "timestamp", "valence"]
+    # merge arousal and valence labels
+    labels = pd.merge(arousal_labels, valence_labels, on=["filename", "timestamp"])
+    # change the dtype of timestamp column to float
+    labels["timestamp"] = labels["timestamp"].astype(float)
+    # extract faces
+    metadata = extract_faces_with_filenames_in_dataframe(df_with_filenames=labels, path_to_data=path_to_data,
+                                              output_path=output_path, every_n_frames=20)
+    # delete nan values from the metadata
+    metadata = metadata.dropna()
+    # save metadata
+    metadata.to_csv(os.path.join(r"G:\Datasets\SEWA", "preprocessed_labels.csv"), index=False)
+
+
+
+if __name__ == '__main__':
+    main()
