d7d62a20622eef37ea558e92d640d865b1988143
Denis <ecodenis@yandex.ru>
2022-12-20 21:29:31 +0100

    still not complete

--- src/training/data_preparation.py
+++ src/training/data_preparation.py
@@ -1,8 +1,22 @@
 import os
+from functools import partial
 from typing import Tuple, List
 
 import pandas as pd
 import numpy as np
+import torch
+import torchvision.transforms as T
+from torch.utils.data import DataLoader
+from transformers import DeiTImageProcessor
+
+
+from pytorch_utils.data_loaders.ImageDataLoader_new import ImageDataLoader
+from pytorch_utils.data_loaders.pytorch_augmentations import pad_image_random_factor, grayscale_image, \
+    collor_jitter_image_random, gaussian_blur_image_random, random_perspective_image, random_rotation_image, \
+    random_crop_image, random_posterize_image, random_adjust_sharpness_image, random_equalize_image, \
+    random_horizontal_flip_image, random_vertical_flip_image
+
+
 
 
 def split_dataset_into_train_dev_test(filenames_labels:pd.DataFrame, percentages:Tuple[int,int,int])->List[pd.DataFrame]:
@@ -38,27 +52,99 @@ def split_dataset_into_train_dev_test(filenames_labels:pd.DataFrame, percentages
 
 
 def load_all_dataframes():
+    # TODO: check it
     path_to_AFEW_VA = r"G:\Datasets\AFEW-VA\AFEW-VA\AFEW-VA\preprocessed".replace("\\", os.sep)
     path_to_AffectNet = r"G:\Datasets\AffectNet\AffectNet\preprocessed".replace("\\", os.sep)
     path_to_RECOLA = r"G:\Datasets\RECOLA\preprocessed".replace("\\", os.sep)
     path_to_SEMAINE = r"G:\Datasets\SEMAINE\preprocessed".replace("\\", os.sep)
     path_to_SEWA = r"G:\Datasets\SEWA\preprocessed".replace("\\", os.sep)
 
+    # load dataframes and add np.NaN labels to columns that are not present in the dataset
     AFEW_VA = pd.read_csv(os.path.join(path_to_AFEW_VA,"labels.csv"))
-    #AffectNet = pd.read_csv(path_to_AffectNet + "labels.csv")
-    #RECOLA = pd.read_csv(path_to_RECOLA + "labels.csv")
-    #SEMAINE = pd.read_csv(path_to_SEMAINE + "labels.csv")
-    #SEWA = pd.read_csv(path_to_SEWA + "labels.csv")
+    RECOLA = pd.read_csv(os.path.join(path_to_RECOLA + "labels.csv"))
+    SEMAINE = pd.read_csv(os.path.join(path_to_SEMAINE + "labels.csv"))
+    SEWA = pd.read_csv(os.path.join(path_to_SEWA + "labels.csv"))
+
+    AFEW_VA["category"] = np.NaN
+    RECOLA["category"] = np.NaN
+    SEMAINE["category"] = np.NaN
+    SEWA["category"] = np.NaN
+
 
     # splitting to train, development, and test sets
     percentages = (80, 10, 10)
     AFEW_VA_train, AFEW_VA_dev, AFEW_VA_test = split_dataset_into_train_dev_test(AFEW_VA, percentages)
+    RECOLA_train, RECOLA_dev, RECOLA_test = split_dataset_into_train_dev_test(RECOLA, percentages)
+    SEMAINE_train, SEMAINE_dev, SEMAINE_test = split_dataset_into_train_dev_test(SEMAINE, percentages)
+    SEWA_train, SEWA_dev, SEWA_test = split_dataset_into_train_dev_test(SEWA, percentages)
+    # for the AffectNet we need to do a splitting separately, since it has no video, just a lot of images
+    AffectNet_train = pd.read_csv(os.path.join(path_to_AffectNet, "train_labels.csv"))
+    AffectNet_dev = pd.read_csv(os.path.join(path_to_AffectNet, "dev_labels.csv"))
 
+    # concatenate all dataframes
+    train = pd.concat([AFEW_VA_train, RECOLA_train, SEMAINE_train, SEWA_train, AffectNet_train])
+    dev = pd.concat([AFEW_VA_dev, RECOLA_dev, SEMAINE_dev, SEWA_dev, AffectNet_dev])
+    test = pd.concat([AFEW_VA_test, RECOLA_test, SEMAINE_test, SEWA_test])
 
-def construct_data_loaders():
+    return (train, dev, test)
+
+# we need it to speed up the preprocessing pipeline
+build_in_preprocessing_function = DeiTImageProcessor.from_pretrained("facebook/deit-base-distilled-patch16-224")
+
+def preprocess_image_DeiT(image:np.ndarray)->torch.Tensor:
+    image = build_in_preprocessing_function(images=image, return_tensors="pt")['pixel_values']
+    return image
+
+def resize_image_to_224(image:np.ndarray)->np.ndarray:
+    # TODO: implement it in a way you wanted: black colour to make the image not distorted, etc.
     pass
 
+def construct_data_loaders(train, dev, test, augment_prob:float=0.05, batch_size:int=32, num_workers:int=8)\
+        ->Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
+    """ # TODO: write a docstring
 
+    :param train:
+    :param dev:
+    :param test:
+    :param augment_prob:
+    :param batch_size:
+    :param num_workers:
+    :return:
+    """
+    augmentation_functions = {
+        pad_image_random_factor: augment_prob,
+        grayscale_image: augment_prob,
+        partial(collor_jitter_image_random, brightness=0.5, hue=0.3, contrast=0.3, saturation=0.3): augment_prob,
+        partial(gaussian_blur_image_random, kernel_size=(5, 9), sigma=(0.1, 5)): augment_prob,
+        random_perspective_image: augment_prob,
+        random_rotation_image: augment_prob,
+        partial(random_crop_image, cropping_factor_limits=(0.7, 0.9)): augment_prob,
+        random_posterize_image: augment_prob,
+        partial(random_adjust_sharpness_image, sharpness_factor_limits=(0.1, 3)): augment_prob,
+        random_equalize_image: augment_prob,
+        random_horizontal_flip_image: augment_prob,
+        random_vertical_flip_image: augment_prob
+    }
+
+    preprocessing_functions=[
+        resize_image_to_224,
+        preprocess_image_DeiT
+    ]
+
+    train_data_loader = ImageDataLoader(paths_with_labels=train, preprocessing_functions=preprocessing_functions,
+                 augmentation_functions=augmentation_functions, shuffle=True)
+
+    dev_data_loader = ImageDataLoader(paths_with_labels=dev, preprocessing_functions=preprocessing_functions,
+                    augmentation_functions=None, shuffle=False)
+
+    test_data_loader = ImageDataLoader(paths_with_labels=test, preprocessing_functions=preprocessing_functions,
+                    augmentation_functions=None, shuffle=False)
+
+    train_dataloader = DataLoader(train_data_loader, batch_size=batch_size, num_workers=num_workers)
+    dev_dataloader = DataLoader(dev_data_loader, batch_size=batch_size, num_workers=num_workers)
+    test_dataloader = DataLoader(test_data_loader, batch_size=batch_size, num_workers=num_workers)
+
+    return (train_dataloader, dev_dataloader, test_dataloader)
 
 if __name__ == "__main__":
     load_all_dataframes()
