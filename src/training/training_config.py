5a407b3ddd4f2184e98731d4bca0b7df282870e8
Denis <ecodenis@yandex.ru>
2022-12-21 19:27:00 +0100

    A new file for training conviguration (training parameters) has been created.

---
+++ src/training/training_config.py
@@ -0,0 +1,14 @@
+NUM_CLASSES:int = 8
+BATCH_SIZE:int = 128
+AUGMENT_PROB:float = 0.05
+
+EMO_CATEGORIES:dict = {
+    "N":0,
+    "H":1,
+    "Sa":2,
+    "Su":3,
+    "F":4,
+    "D":5,
+    "A":6,
+    "C":7,
+}
