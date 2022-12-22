9e3fdbe7b48c0a863a723bd5eae18ab8846663a3
Denis <ecodenis@yandex.ru>
2022-12-22 15:53:17 +0100

    New parameters have been added.

--- src/training/training_config.py
+++ src/training/training_config.py
@@ -1,11 +1,13 @@
 NUM_CLASSES:int = 8
-EPOCHS:int = 100
+NUM_EPOCHS:int = 100
 BATCH_SIZE:int = 128
 AUGMENT_PROB:float = 0.05
 OPTIMIZER:str = "Adam"
-LR_MAX:float = 0.001
+LR_MAX:float = 0.005
+LR_MIN:float = 0.0001
 LR_SCHEDULLER:str = "Cyclic"
 ANNEALING_PERIOD:int = 6
+BEST_MODEL_SAVE_PATH:str = "best_model/"
 
 
 
