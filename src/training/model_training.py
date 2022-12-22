f5001fb15bfc8ec6126f487b1bf9feeb9ea7d90f
Denis <ecodenis@yandex.ru>
2022-12-22 15:53:24 +0100

    tmp

--- src/training/model_training.py
+++ src/training/model_training.py
@@ -1,10 +1,12 @@
+import gc
+import os
 from functools import partial
-from typing import Tuple, List, Optional
+from typing import Tuple, List, Optional, Dict
 
 import pandas as pd
 import numpy as np
 import torch
-from sklearn.metrics import recall_score, precision_score, f1_score
+from sklearn.metrics import recall_score, precision_score, f1_score, accuracy_score, mean_squared_error
 from torch.nn.functional import one_hot
 from torchinfo import summary
 
@@ -12,6 +14,79 @@ import training_config
 from pytorch_utils.training_utils.callbacks import TorchEarlyStopping, TorchMetricEvaluator
 from pytorch_utils.training_utils.losses import SoftFocalLoss
 from src.models.ViT_models import ViT_Deit_model
+
+
+
+def evaluate_model(model:torch.nn.Module, generator:torch.utils.data.DataLoader, device:torch.device) -> Tuple[Dict[object, float],...]:
+
+    evaluation_metrics_classification = { 'accuracy': accuracy_score,
+                                          'precision': partial(precision_score, average='macro'),
+                                          'recall': partial(recall_score, average='macro'),
+                                          'f1': partial(f1_score, average='macro')
+                                        }
+
+    evaluation_metric_arousal = {'mse': mean_squared_error,
+                                 'mae': partial(mean_squared_error, squared=False)
+                                 }
+
+    evaluation_metric_valence = {'mse': mean_squared_error,
+                                 'mae': partial(mean_squared_error, squared=False)
+                                 }
+    # create arrays for predictions and ground truth labels
+    predictions_classifier, predictions_arousal, predictions_valence = []
+    ground_truth_classifier, ground_truth_arousal, ground_truth_valence = []
+
+    # start evaluation
+    model.eval()
+    with torch.no_grad():
+        for i, data in enumerate(generator):
+            # get the inputs; data is a list of [inputs, labels]
+            inputs, labels = data
+            inputs = inputs.float()
+            inputs = inputs.to(device)
+
+            # separate labels into a list of torch.Tensor
+            labels = [labels[:,0], labels[:,1], labels[:,2]]
+            labels[2] = one_hot(labels[2].long(), num_classes=training_config.NUM_CLASSES)
+            labels = [label.float().to(device) for label in labels]
+
+            # forward pass
+            output_arousal = output_arousal.cpu().numpy().squeeze()
+            output_valence = output_valence.cpu().numpy().squeeze()
+            output_classifier, output_arousal, output_valence = model(inputs)
+            output_classifier = torch.softmax(output_classifier, dim=-1)
+            output_classifier = torch.argmax(output_classifier, dim=-1)
+            output_classifier = output_classifier.cpu().numpy().squeeze()
+            # save ground_truth labels and predictions in arrays
+            predictions_arousal.append(output_arousal)
+            predictions_valence.append(output_valence)
+            predictions_classifier.append(output_classifier)
+            ground_truth_arousal.append(labels[0].cpu().numpy().squeeze())
+            ground_truth_valence.append(labels[1].cpu().numpy().squeeze())
+            ground_truth_classifier.append(torch.argmax(labels[2], dim=-1).cpu().numpy().squeeze())
+        # concatenate evaluation metrics
+        predictions_arousal = np.concatenate(predictions_arousal)
+        predictions_valence = np.concatenate(predictions_valence)
+        predictions_classifier = np.concatenate(predictions_classifier)
+        ground_truth_arousal = np.concatenate(ground_truth_arousal)
+        ground_truth_valence = np.concatenate(ground_truth_valence)
+        ground_truth_classifier = np.concatenate(ground_truth_classifier)
+        # calculate evaluation metrics
+        evaluation_metrics_arousal = {metric: evaluation_metric_arousal[metric](ground_truth_arousal, predictions_arousal) for metric in evaluation_metric_arousal}
+        evaluation_metrics_valence = {metric: evaluation_metric_valence[metric](ground_truth_valence, predictions_valence) for metric in evaluation_metric_valence}
+        evaluation_metrics_classifier = {metric: evaluation_metrics_classification[metric](ground_truth_classifier, predictions_classifier) for metric in evaluation_metrics_classification}
+        # print evaluation metrics
+        print('Evaluation metrics for arousal:')
+        for metric_name, metric_value in evaluation_metrics_arousal.items():
+            print("%s: %.4f" % (metric_name, metric_value))
+        print('Evaluation metrics for valence:')
+        for metric_name, metric_value in evaluation_metrics_valence.items():
+            print("%s: %.4f" % (metric_name, metric_value))
+        print('Evaluation metrics for classifier:')
+        for metric_name, metric_value in evaluation_metrics_classifier.items():
+            print("%s: %.4f" % (metric_name, metric_value))
+    return (evaluation_metrics_arousal, evaluation_metrics_valence, evaluation_metrics_classifier)
+
 
 
 def train_step(model:torch.nn.Module, optimizer:torch.optim.Optimizer, criterion:Tuple[torch.nn.Module,...],
@@ -98,14 +173,14 @@ def train_epoch(model:torch.nn.Module, train_generator:torch.utils.data.DataLoad
     return total_loss / counter
 
 
-def train_model(model:torch.nn.Module, train_generator:torch.utils.data.DataLoader, dev_generator:torch.utils.data.DataLoader,
-                device:torch.device) -> None:
+def train_model(train_generator:torch.utils.data.DataLoader, dev_generator:torch.utils.data.DataLoader,
+                class_weights:torch.Tensor) -> None:
 
     # create model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = ViT_Deit_model(num_classes=training_config.NUM_CLASSES)
     model = model.to(device)
-    summary(model, input_size=(config.batch_size, 3, 256, 256))
+    summary(model, input_size=(training_config.BATCH_SIZE, 3, 256, 256))
     # select optimizer
     optimizers = {'Adam': torch.optim.Adam,
                   'SGD': torch.optim.SGD,
@@ -116,8 +191,8 @@ def train_model(model:torch.nn.Module, train_generator:torch.utils.data.DataLoad
     criterions = (torch.nn.MSELoss(), torch.nn.MSELoss(), SoftFocalLoss(softmax=True, alpha=class_weights, gamma=2))
     # create LR scheduler
     lr_schedullers = {
-        'Cyclic': torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.annealing_period,
-                                                             eta_min=config.learning_rate_min),
+        'Cyclic': torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=training_config.ANNEALING_PERIOD,
+                                                             eta_min=training_config.LR_MIN),
         'ReduceLRonPlateau': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=8),
     }
     lr_scheduller = lr_schedullers[training_config.LR_SCHEDULLER]
@@ -127,12 +202,40 @@ def train_model(model:torch.nn.Module, train_generator:torch.utils.data.DataLoad
         'val_precision': partial(precision_score, average='macro'),
         'val_f1_score': partial(f1_score, average='macro')
     }
-    best_val_recall = 0
+    best_val_metric_value = np.inf # we do minimization
     early_stopping_callback = TorchEarlyStopping(verbose=True, patience=15,
                                                  save_path='best_model/',
                                                  mode="min")
 
-    # TODO: write an evaluation loop
+    # train model
+    for epoch in range(training_config.NUM_EPOCHS):
+        print("Epoch: %i" % epoch)
+        # train
+        model.train()
+        train_loss = train_epoch(model, train_generator, optimizer, criterions, device, print_step=100)
+        print("Train loss: %.10f" % train_loss)
+        # validate
+        model.eval()
+        print("Evaluation of the model on dev set.")
+        val_metric_arousal, val_metric_valence, val_metrics_classification = evaluate_model(model, dev_generator, device)
+        metric_value = val_metric_arousal['mse']+val_metric_valence['mse']+(1-val_metrics_classification['recall'])
+        # update LR
+        if training_config.LR_SCHEDULLER == 'ReduceLRonPlateau':
+            lr_scheduller.step(metric_value)
+        else:
+            lr_scheduller.step()
+        # save best model
+        if metric_value < best_val_metric_value:
+            best_val_metric_value = metric_value
+            torch.save(model.state_dict(), os.path.join(training_config.BEST_MODEL_SAVE_PATH, 'best_model.pth'))
+        # check early stopping
+        early_stopping_result = early_stopping_callback(metric_value, model)
+        if early_stopping_result:
+            print("Early stopping")
+            break
+    # clear RAM
+    gc.collect()
+    torch.cuda.empty_cache()
 
 
 
@@ -161,99 +264,5 @@ def train_model(model:torch.nn.Module, train_generator:torch.utils.data.DataLoad
 
 
 if __name__ == "__main__":
-
-    # initialization of Weights and Biases
-    wandb.init(project="VGGFace2_FtF_training", config=metaparams)
-    config = wandb.config
-
-    # create model
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    HRNet = load_HRNet_model(device="cuda" if torch.cuda.is_available() else "cpu",
-                             path_to_weights="/work/home/dsu/simpleHRNet/pose_hrnet_w32_256x192.pth")
-    model = modified_HRNet(HRNet, num_classes=config.num_classes)
-    model.to(device)
-    summary(model, input_size=(config.batch_size, 3, 256, 256))
-    # Select optimizer
-    optimizers = {'Adam': torch.optim.Adam,
-                  'SGD': torch.optim.SGD,
-                  'RMSprop': torch.optim.RMSprop,
-                  'AdamW': torch.optim.AdamW}
-
-    optimizer = optimizers[config.optimizer](model.parameters(), lr=config.learning_rate_max)
-    # select loss function
-    class_weights = torch.from_numpy(class_weights).float()
-    class_weights = class_weights.to(device)
-    criterions = {'Crossentropy': torch.nn.CrossEntropyLoss(weight=class_weights),
-                  'Focal_loss': SoftFocalLoss(softmax=True, alpha=class_weights, gamma=2)}
-    criterion = criterions[loss_function]
-    wandb.config.update({'loss': criterion})
-    # Select lr scheduller
-    lr_schedullers = {
-        'Cyclic': torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=config.annealing_period,
-                                                             eta_min=config.learning_rate_min),
-        'ReduceLRonPlateau': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='max', patience=8),
-    }
-    lr_scheduller = lr_schedullers[config.lr_scheduller]
-    # callbacks
-    val_metrics = {
-        'val_recall': partial(recall_score, average='macro'),
-        'val_precision': partial(precision_score, average='macro'),
-        'val_f1_score': partial(f1_score, average='macro')
-    }
-    best_val_recall = 0
-    early_stopping_callback = TorchEarlyStopping(verbose=True, patience=15,
-                                                 save_path=wandb.run.dir,
-                                                 mode="max")
-
-    metric_evaluator = TorchMetricEvaluator(generator=dev,
-                                            model=model,
-                                            metrics=val_metrics,
-                                            device=device,
-                                            output_argmax=True,
-                                            output_softmax=True,
-                                            labels_argmax=True,
-                                            loss_func=criterion)
-    # print information about run
-    print('Metaparams: optimizer: %s, learning_rate:%f, lr_scheduller: %s, annealing_period: %d, epochs: %d, '
-          'batch_size: %d, augmentation_rate: %f, architecture: %s, '
-          'dataset: %s, num_classes: %d' % (config.optimizer, config.learning_rate_max, config.lr_scheduller,
-                                            config.annealing_period, config.epochs, config.batch_size,
-                                            config.augmentation_rate, config.architecture, config.dataset,
-                                            config.num_classes))
-    # go through epochs
-    for epoch in range(epochs):
-        # train model one epoch
-        loss = train_step(model=model, train_generator=train, optimizer=optimizer, criterion=criterion,
-                          device=device, print_step=100)
-        loss = loss / config.batch_size
-
-        # evaluate model on dev set
-        print('model evaluation...')
-        with torch.no_grad():
-            dev_results = metric_evaluator()
-            print("Epoch: %i, dev results:" % epoch)
-            for metric_name, metric_value in dev_results.items():
-                print("%s: %.4f" % (metric_name, metric_value))
-            # check early stopping
-            early_stopping_result = early_stopping_callback(dev_results['val_recall'], model)
-            # check if we have new best recall result on the validation set
-            if dev_results['val_recall'] > best_val_recall:
-                best_val_recall = dev_results['val_recall']
-                wandb.config.update({'best_val_recall': best_val_recall}, allow_val_change=True)
-        # log everything using wandb
-        wandb.log({'epoch': epoch}, commit=False)
-        wandb.log({'learning_rate': optimizer.param_groups[0]["lr"]}, commit=False)
-        wandb.log(dev_results, commit=False)
-        wandb.log({'train_loss': loss})
-        # update lr
-        if config.lr_scheduller == "ReduceLRonPlateau":
-            lr_scheduller.step(dev_results['loss'])
-        elif config.lr_scheduller == "Cyclic":
-            lr_scheduller.step()
-        # break the training loop if the model is not improving for a while
-        if early_stopping_result:
-            break
-    # clear RAM
-    gc.collect()
-    torch.cuda.empty_cache()
+    pass
 
