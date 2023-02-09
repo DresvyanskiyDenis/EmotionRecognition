378141834c60411ba8ca5bbf5c8cbef4e2952194
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2023-02-09 19:06:11 +0100

    New training meta-params have been added.

--- src/training/training_config.py
+++ src/training/training_config.py
@@ -1,19 +1,33 @@
 from typing import Tuple
 
+# model architecture params
 NUM_CLASSES:int = 8
 NUM_REGRESSION_NEURONS:int = 2
+IMAGE_RESOLUTION:Tuple[int, int] = (224, 224)
+
+# training metaparams
 NUM_EPOCHS:int = 100
 BATCH_SIZE:int = 128
-AUGMENT_PROB:float = 0.05
 OPTIMIZER:str = "Adam"
-LR_MAX:float = 0.005
-LR_MIN:float = 0.0001
+AUGMENT_PROB:float = 0.05
+EARLY_STOPPING_PATIENCE:int = 15
+
+# scheduller
 LR_SCHEDULLER:str = "Cyclic"
 ANNEALING_PERIOD:int = 6
-BEST_MODEL_SAVE_PATH:str = "best_model/"
+LR_MAX_CYCLIC:float = 0.005
+LR_MIN_CYCLIC:float = 0.001
+LR_MIN_WARMUP:float = 0.0001
+WARMUP_STEPS:int = 200
+WARMUP_MODE:str = "linear"
 
-IMAGE_RESOLUTION:Tuple[int, int] = (224, 224)
+# general params
+BEST_MODEL_SAVE_PATH:str = "best_model/"
 NUM_WORKERS:int = 8
+
+
+
+
 
 EMO_CATEGORIES:dict = {
     "N":0,
