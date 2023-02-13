5aa523cfc350a305c6cf35232a66be55da63ac63
Dresvyanskiy <denis.dresvyanskiy@uni-ulm.de>
2023-02-13 12:16:40 +0100

    Several training params have been changed.

--- src/training/training_config.py
+++ src/training/training_config.py
@@ -7,23 +7,23 @@ IMAGE_RESOLUTION:Tuple[int, int] = (224, 224)
 
 # training metaparams
 NUM_EPOCHS:int = 100
-BATCH_SIZE:int = 128
+BATCH_SIZE:int = 256
 OPTIMIZER:str = "Adam"
 AUGMENT_PROB:float = 0.05
-EARLY_STOPPING_PATIENCE:int = 15
+EARLY_STOPPING_PATIENCE:int = 25
 WEIGHT_DECAY:float = 0.00001
 
 # scheduller
 LR_SCHEDULLER:str = "Warmup_cyclic"
-ANNEALING_PERIOD:int = 5
+ANNEALING_PERIOD:int = 4
 LR_MAX_CYCLIC:float = 0.005
 LR_MIN_CYCLIC:float = 0.0005
 LR_MIN_WARMUP:float = 0.0001
-WARMUP_STEPS:int = 200
+WARMUP_STEPS:int = 100
 WARMUP_MODE:str = "linear"
 
 # gradual unfreezing
-UNFREEZING_LAYERS_PER_EPOCH:int = 2
+UNFREEZING_LAYERS_PER_EPOCH:int = 1
 LAYERS_TO_UNFREEZE_BEFORE_START:int = 7
 
 # general params
