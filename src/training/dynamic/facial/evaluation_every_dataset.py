336a6baaef71c001cd544e5e9273aa481141f07a
Denis <dresvyanskiydenis@gmail.com>
2023-12-05 22:56:17 +0100

    Small code cleaning

--- src/training/dynamic/facial/evaluation_every_dataset.py
+++ src/training/dynamic/facial/evaluation_every_dataset.py
@@ -50,14 +50,14 @@ def get_info_and_download_models_weights_from_project(entity: str, project_name:
         val_RMSE = run.summary['val_RMSE']
         # pack the info into the DataFrame
         info = pd.concat([info,
-                            pd.DataFrame.from_dict(
-                                {'ID': [ID], 'base_model_type': [base_model_type], 'architecture': [architecture],
-                                 'sequence_length': [sequence_length], 'val_RMSE_arousal': [val_RMSE_arousal],
-                                 'val_RMSE_valence': [val_RMSE_valence], 'val_MAE_arousal': [val_MAE_arousal],
-                                 'val_MAE_valence': [val_MAE_valence], 'val_RMSE': [val_RMSE]}
-                            )
-                            ]
-                             )
+                          pd.DataFrame.from_dict(
+                              {'ID': [ID], 'base_model_type': [base_model_type], 'architecture': [architecture],
+                               'sequence_length': [sequence_length], 'val_RMSE_arousal': [val_RMSE_arousal],
+                               'val_RMSE_valence': [val_RMSE_valence], 'val_MAE_arousal': [val_MAE_arousal],
+                               'val_MAE_valence': [val_MAE_valence], 'val_RMSE': [val_RMSE]}
+                          )
+                          ]
+                         )
         # download the model weights
         final_output_path = os.path.join(output_path, ID)
         run.file('best_model.pth').download(final_output_path, replace=True)
@@ -68,31 +68,20 @@ def get_info_and_download_models_weights_from_project(entity: str, project_name:
         os.rmdir(final_output_path)
     return info
 
-def get_CCC_data_loaders_for_dataset(dataset_name:str, window_size:int):
+
+def get_data_loaders_for_separate_dataset(dataset_name: str, window_size: int, stride: int):
     # load data and separate it on videos
     train, dev, test = load_one_dataset(dataset_name)
     train, dev, test = separate_various_videos(train), separate_various_videos(dev), separate_various_videos(test)
     # construct data loaders
     train_dataloader, dev_dataloader, test_dataloader = construct_data_loaders(train, dev, test,
                                                                                window_size=window_size,
-                                                                               stride=1,
+                                                                               stride=stride,
                                                                                preprocessing_functions=None,
                                                                                batch_size=32,
                                                                                augmentation_functions=None)
     return (train_dataloader, dev_dataloader, test_dataloader)
 
-def get_normal_data_loaders_for_dataset(dataset_name:str, window_size:int):
-    # load data and separate it on videos
-    train, dev, test = load_one_dataset(dataset_name)
-    train, dev, test = separate_various_videos(train), separate_various_videos(dev), separate_various_videos(test)
-    # construct data loaders
-    train_dataloader, dev_dataloader, test_dataloader = construct_data_loaders(train, dev, test,
-                                                                               window_size=window_size,
-                                                                               stride=int(window_size*0.4),
-                                                                               preprocessing_functions=None,
-                                                                               batch_size=32,
-                                                                               augmentation_functions=None)
-    return (train_dataloader, dev_dataloader, test_dataloader)
 
 def main():
     # set the project name
@@ -104,13 +93,13 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     # metrics
     n_metrics = {'MSE_val_arousal': mean_squared_error,
-               'MAE_val_arousal': mean_absolute_error,
-               'MSE_val_valence': mean_squared_error,
-               'MAE_val_valence': mean_absolute_error,
-               }
+                 'MAE_val_arousal': mean_absolute_error,
+                 'MSE_val_valence': mean_squared_error,
+                 'MAE_val_valence': mean_absolute_error,
+                 }
     CCC_metrics = {'CCC_val_arousal': CCC,
-                     'CCC_val_valence': CCC,
-                     }
+                   'CCC_val_valence': CCC,
+                   }
     # get the info about the models and download the models weights
     info = get_info_and_download_models_weights_from_project(entity=entity,
                                                              project_name=project_name,
@@ -143,49 +132,52 @@ def main():
         # go through all datasets
         for dataset_name in datasets:
             n_train_generator, n_dev_generator, n_test_generator = \
-                get_normal_data_loaders_for_dataset(dataset_name=dataset_name, window_size=info['sequence_length'].iloc[i])
+                get_data_loaders_for_separate_dataset(dataset_name=dataset_name,
+                                                      window_size=info['sequence_length'].iloc[i],
+                                                      stride=int(info['sequence_length'].iloc[i] * 0.4))
             ccc_train_generator, ccc_dev_generator, ccc_test_generator = \
-                get_CCC_data_loaders_for_dataset(dataset_name=dataset_name, window_size=info['sequence_length'].iloc[i])
+                get_data_loaders_for_separate_dataset(dataset_name=dataset_name,
+                                                      window_size=info['sequence_length'].iloc[i],
+                                                      stride=1)
             # evaluate the model for getting RMSE and MAE
             n_dev_metrics = evaluate_model(model=model, data_generator=n_dev_generator,
-                                         metrics=n_metrics, device=device)
+                                           metrics=n_metrics, device=device)
             n_test_metrics = evaluate_model(model=model, data_generator=n_test_generator,
-                                          metrics=n_metrics, device=device)
+                                            metrics=n_metrics, device=device)
             # add to all metrics name the 'val_' or 'test_' prefix (depending on the data_generator)
             n_dev_metrics = {key.replace('dev_', ''): value for key, value in n_dev_metrics.items()}
             n_test_metrics = {key.replace('test_', ''): value for key, value in n_test_metrics.items()}
 
             # evaluate the model for getting CCC
             CCC_dev_metrics = evaluate_model(model=model, data_generator=ccc_dev_generator,
-                                            metrics=CCC_metrics, device=device)
+                                             metrics=CCC_metrics, device=device)
             CCC_test_metrics = evaluate_model(model=model, data_generator=ccc_test_generator,
-                                                metrics=CCC_metrics, device=device)
+                                              metrics=CCC_metrics, device=device)
             # add to all metrics name the 'val_' or 'test_' prefix (depending on the data_generator)
             CCC_dev_metrics = {key.replace('dev_', ''): value for key, value in CCC_dev_metrics.items()}
             CCC_test_metrics = {key.replace('test_', ''): value for key, value in CCC_test_metrics.items()}
 
             # put everything in the result_info dataframe
             new_row = pd.DataFrame.from_dict({'ID': info['ID'].iloc[i],
-                                                'base_model_type': info['base_model_type'].iloc[i],
-                                                'architecture': info['architecture'].iloc[i],
-                                                'sequence_length': info['sequence_length'].iloc[i],
-                                                'dataset': dataset_name,
-                                                'val_RMSE_arousal': n_dev_metrics['RMSE_arousal'],
-                                                'val_RMSE_valence': n_dev_metrics['RMSE_valence'],
-                                                'val_MAE_arousal': n_dev_metrics['MAE_arousal'],
-                                                'val_MAE_valence': n_dev_metrics['MAE_valence'],
-                                                'RMSE_dev_arousal': n_test_metrics['RMSE_arousal'],
-                                                'RMSE_dev_valence': n_test_metrics['RMSE_valence'],
-                                                'CCC_dev_arousal': CCC_dev_metrics['CCC_arousal'],
-                                                'CCC_dev_valence': CCC_dev_metrics['CCC_valence'],
-                                                'CCC_test_arousal': CCC_test_metrics['CCC_arousal'],
-                                                'CCC_test_valence': CCC_test_metrics['CCC_valence'],
-                                                })
+                                              'base_model_type': info['base_model_type'].iloc[i],
+                                              'architecture': info['architecture'].iloc[i],
+                                              'sequence_length': info['sequence_length'].iloc[i],
+                                              'dataset': dataset_name,
+                                              'val_RMSE_arousal': n_dev_metrics['RMSE_arousal'],
+                                              'val_RMSE_valence': n_dev_metrics['RMSE_valence'],
+                                              'val_MAE_arousal': n_dev_metrics['MAE_arousal'],
+                                              'val_MAE_valence': n_dev_metrics['MAE_valence'],
+                                              'RMSE_dev_arousal': n_test_metrics['RMSE_arousal'],
+                                              'RMSE_dev_valence': n_test_metrics['RMSE_valence'],
+                                              'CCC_dev_arousal': CCC_dev_metrics['CCC_arousal'],
+                                              'CCC_dev_valence': CCC_dev_metrics['CCC_valence'],
+                                              'CCC_test_arousal': CCC_test_metrics['CCC_arousal'],
+                                              'CCC_test_valence': CCC_test_metrics['CCC_valence'],
+                                              })
             result_info = pd.concat([result_info, new_row], ignore_index=True)
 
         # save the result_info DataFrame
         result_info.to_csv(os.path.join(output_path, 'result_info_separate_datasets.csv'), index=False)
-
 
 
 if __name__ == '__main__':
